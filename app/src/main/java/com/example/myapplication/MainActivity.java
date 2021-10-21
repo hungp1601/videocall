db0a5046059830b3f52a1dfacbedbd1c563f151f
hungp1601 <59797651+hungp1601@users.noreply.github.com>
2021-10-21 09:07:37 +0700

    add navbar

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView navView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+    }
+
+
+
+    public BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener;
+
+    {
+        navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_home:
+                        Intent mainIntent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(mainIntent);
+                        break;
+                    case R.id.navigation_settings:
+                        Intent settingsIntent = new Intent(MainActivity.this, SettingsActivity.class);
+                        startActivity(settingsIntent);
+                        break;
+                    case R.id.navigation_notifications:
+                        Intent notificationIntent = new Intent(MainActivity.this, NotificationsActivity.class);
+                        startActivity(notificationIntent);
+                        break;
+                    case R.id.navigation_logout:
+                        FirebaseAuth.getInstance().signOut();
+                        Intent logoutIntent = new Intent(MainActivity.this, RegistrationActivity.class);
+                        startActivity(logoutIntent);
+                        finish();
+                        break;
+                }
+                return true;
+            }
+
+        };
     }
 }
--- app/src/main/java/com/example/myapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/myapplication/RegistrationActivity.java
@@ -1,8 +1,5 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,11 +9,15 @@ import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.hbb20.CountryCodePicker;
@@ -112,6 +113,13 @@ public class RegistrationActivity extends AppCompatActivity {
         relativeLayout = findViewById(R.id.phoneAuth);
         ccp = (CountryCodePicker) findViewById(R.id.ccp);
         ccp.registerCarrierNumberEditText(phoneText);
+    }
+    protected void onStart(){
+        super.onStart();
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(firebaseUser!=null){
+            sendUserToMainActivity();
+        }
     }
     private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
         mAuth.signInWithCredential(credential)
