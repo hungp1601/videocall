916116bf69461262cee6ec0a71a2ed77b79dab15
thuhang2510 <tunguyet2510@gmail.com>
2021-11-03 16:08:27 +0700

    Them giao dien goi video

---
+++ app/src/main/java/com/example/vivid/CallingActivity.java
@@ -0,0 +1,14 @@
+package com.example.vivid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CallingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calling);
+    }
+}
